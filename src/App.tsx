3e5e8d06de48c27e90945a7bd1a4628514fae52a
Bhuvan-S-prasad <bhuvansprasad20@gmail.com>
2026-01-29 16:00:46 +0530

    feat(hero): add GSAP-animated hero section with smooth intro transitions

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <>
       <div className='relative w-screen min-h-screen overflow-x-auto'>
         <Navbar />
+        <Hero />  
       </div>
     </>
   )
